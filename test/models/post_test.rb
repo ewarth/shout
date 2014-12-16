94f86c480836bde300765ebdca529cc2a6db619b
hmcginnis <mcginnis.harold@gmail.com>
2014-12-16 14:43:16 -0500

    add tests for models

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Shout Not Add Post too short" do
+    @user = User.find_by_id(1)
+    @text = ''
+    @post = Post.new(:user_id => @user, :post_text => @text)
+    assert_not(@post.save, 'should have erred')
+    @errors = @post.errors
+    @error =  @errors[:post_text].first
+    assert(@error == 'can\'t be blank', @error + ' should have been "can\'t be blank"')
+  end
+
+  test "Shout Not Add Post invalid chars" do
+    @user = User.find_by_id(1)
+    @text = '<>'
+    @post = Post.new(:user_id => @user, :post_text => @text)
+    assert_not(@post.save, 'should have erred')
+    @errors = @post.errors
+    @error =  @errors[:post_text].first
+    assert(@error == 'Invalid characters are contained in the post', @error + ' should have been "Invalid characters are contained in the post"')
+  end
+
+  test "Shout Add Post" do
+    @user = User.find_by_id(1)
+    @text = 'valid message'
+    @post = Post.new(:user_id => @user, :post_text => @text)
+    assert(@post.save, 'should have saved')
+    @errors = @post.errors
+    assert(@errors.empty?, 'should not have erred')
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Shout Not Add User bad fnmae" do
+    @user = User.new(:email => 'test@test.com', :first_name => "<>", :last_name => "test", :encrypted_password => "something")
+
+    assert_not(@user.save, 'should have erred')
+    @errors = @user.errors
+    @error =  @errors[:first_name].first
+    assert(@error == 'Only alphanumerics and "." allowed', @error + ' should have been "Only alphanumerics and "." allowed"')
+  end
+
+  test "Shout Not Add User bad lnmae" do
+    @user = User.new(:email => 'test@test.com', :first_name => "test", :last_name => "<>", :encrypted_password => "something")
+
+    assert_not(@user.save, 'should have erred')
+    @errors = @user.errors
+    @error =  @errors[:last_name].first
+    assert(@error == 'Only alphanumerics and "." allowed', @error + ' should have been "Only alphanumerics and "." allowed"')
+  end
+
+  test "Shout Not Add User bad email" do
+    @user = User.new(:email => 'test', :first_name => "test", :last_name => "test", :encrypted_password => "something")
+
+    assert_not(@user.save, 'should have erred')
+    @errors = @user.errors
+    @error =  @errors[:email].first
+    assert(@error == 'is invalid', @error + ' should have been "is invalid"')
+  end
 end
