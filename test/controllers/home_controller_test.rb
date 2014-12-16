c5aa6a0ddc446f0bb95097e4e86b5af53517a017
hmcginnis <mcginnis.harold@gmail.com>
2014-12-16 16:01:28 -0500

    add tests for controllers

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,24 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
+  test "should get friendships show" do
+   login_user
+    get :index
+    assert_redirected_to(controller: "friendships", action: "show")
+  end
+
+
+  def login_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = User.find_by_id(1)
+    sign_in user
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,34 +1,57 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+  test "should get new" do
+    login_user
+    get :new
+    assert_response :success
+  end
+
   test "should get create" do
-    get :create
+    login_user
+    get(:create, post: {post_text: 'test'})
+    assert_redirected_to(controller: "friendships", action: "show")
+  end
+
+  test "should get show" do
+    login_user
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    login_user
+    get(:edit, post_id: Post.find_by_id(1))
     assert_response :success
   end
 
-  test "should get share" do
-    get :share
-    assert_response :success
+  test "should get change" do
+    login_user
+    get(:change, post:{id: 1 , post_text: "test"})
+    assert_redirected_to(controller: "posts", action: "show")
   end
 
   test "should get favorite" do
-    get :favorite
-    assert_response :success
+    login_user
+    get(:favorite, post_id: Post.find_by_id(1))
+    assert_redirected_to(controller: "friendships", action: "show")
   end
 
   test "should get report" do
-    get :report
-    assert_response :success
+    login_user
+    get(:report, post_id: Post.find_by_id(1))
+    assert_redirected_to(controller: "friendships", action: "show")
   end
 
   test "should get delete" do
-    get :delete
-    assert_response :success
+    login_user
+    get(:delete, post_id: Post.find_by_id(1))
+    assert_redirected_to(controller: "posts", action: "show")
+  end
+
+  def login_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = User.find_by_id(1)
+    sign_in user
   end
 
 end
